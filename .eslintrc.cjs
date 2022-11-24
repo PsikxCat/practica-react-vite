77990652caa15f63a92545846e97bf3ad2f8bb93
PsikxCat <arevalogrichard@gmail.com>
2022-11-24 15:30:22 -0500

    🎉 Inicio de proyecto

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+  }
+};
